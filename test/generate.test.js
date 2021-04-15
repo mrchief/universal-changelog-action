6a928d16e1080431197fd53e47967604935d14f0
Soumya Ranjan Mahunt <devsoumyamahunt@gmail.com>
2021-04-15 16:48:34 +0530

    feat: add test cases for commit detection

--- test/generate.test.js
+++ test/generate.test.js
@@ -1,7 +1,8 @@
 const { generate } = require("../lib/generate")
 const commits = require("./fixtures/commits.json")
+const comparison = require("./fixtures/comparison.json")
 
-test("generates changelog", async () => {
+test("generates changelog from commits", async () => {
   const previousReleaseTagNameOrSha = "2020-07-26T191652Z"
   const nextReleaseTagName = "2020-08-02T034148Z"
   const nextReleaseName = "Release 2020-08-02T034148Z"
@@ -12,5 +13,21 @@ test("generates changelog", async () => {
     commits,
     nextReleaseName,
   })
+
+  expect(changelog).toMatchSnapshot()
+})
+
+test("generates changelog from refs", async () => {
+  const previousReleaseTagNameOrSha = "2020-07-26T191652Z"
+  const nextReleaseTagName = "2020-08-02T034148Z"
+  const nextReleaseName = "Release 2020-08-02T034148Z"
+
+  const changelog = await generate({
+    nextReleaseTagName,
+    previousReleaseTagNameOrSha,
+    commits: comparison.commits,
+    nextReleaseName,
+  })
+
   expect(changelog).toMatchSnapshot()
 })
