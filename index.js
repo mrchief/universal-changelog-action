dad8c65af5b413beeed052f669834825fe6ee100
Hrusikesh Panda <mrchief_2000@yahoo.com>
2021-05-25 21:19:34 -0400

    fix: switch to list commits

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
-const core = require("@actions/core")
+const core = require('@actions/core')
 const github = require('@actions/github')
-const { Octokit } = require("@octokit/rest")
-const { generate } = require("./lib/generate")
+const { Octokit } = require('@octokit/rest')
+const { generate } = require('./lib/generate')
 
 async function run() {
   try {
-    const octokit = new Octokit();
+    const octokit = new Octokit()
 
-    const previousReleaseTagNameOrSha = core.getInput("previousReleaseTagNameOrSha")
-    const nextReleaseTagName = core.getInput("nextReleaseTagName")
-    const nextReleaseName = core.getInput("nextReleaseName")
-    const configPath = core.getInput("configFilePath")
-    
+    const previousReleaseTagNameOrSha = core.getInput('previousReleaseTagNameOrSha')
+    const nextReleaseTagName = core.getInput('nextReleaseTagName')
+    const nextReleaseName = core.getInput('nextReleaseName')
+    const configPath = core.getInput('configFilePath')
+
     let commits
-    const commitsJSON = core.getInput("commits")
+    const commitsJSON = core.getInput('commits')
     if (commitsJSON) {
+      core.debug('Received commits')
       commits = JSON.parse(commitsJSON)
     } else {
+      core.debug(`Fetching commits from ${previousReleaseTagNameOrSha}`)
       commits = await octokit.paginate(
-        octokit.repos.compareCommits,
+        octokit.repos.listCommits,
         {
           ...github.context.repo,
-          base: previousReleaseTagNameOrSha,
-          head: nextReleaseTagName,
+          sha: previousReleaseTagNameOrSha,
           per_page: 100,
         },
         (response) => response.data.commits
-        );
+      )
     }
-
+    core.debug('Generating changelog')
     const changelog = await generate({
       nextReleaseTagName,
       previousReleaseTagNameOrSha,
@@ -36,7 +37,7 @@ async function run() {
       commits,
       nextReleaseName,
     })
-    core.setOutput("changelog", changelog)
+    core.setOutput('changelog', changelog)
   } catch (error) {
     core.setFailed(error.message)
   }
